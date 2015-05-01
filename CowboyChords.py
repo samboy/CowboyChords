61191a596bbda07bebb6e501c66504652416244d
Sam Trenholme <strenholme.usenet@gmail.com>
2015-04-30 20:22:05 -0700

    OK, code to find out which notes a strum hits is done

--- CowboyChords.py
+++ CowboyChords.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+OCTAVE = 12
+MAJOR = [1,0,0,0,1,0,0,1,0,0,0,0]
+MINOR = [1,0,0,1,0,0,0,1,0,0,0,0]
 
 # A tuning is an array of 6 numbers between 0 and 11, corresponding to
 # notes in a 12-tone chromatic scale.  0 is E, 1 is F, 2 is F#, 3 is G, etc.
@@ -9,11 +14,19 @@
 # but the "low E" string; 2: Strum just DGBE four high strings)
 
 def isTriad(tuning, strum):
-	for a in range(strum, 6):
-		print tuning[a]
+	min = OCTAVE
+	notes = []
+	for a in range(OCTAVE):
+		notes.append(0)
+	for a in range(strum, len(tuning)):
+		if tuning[a] % OCTAVE < min:
+			min = tuning[a] % OCTAVE
+	for a in range(strum, len(tuning)):
+		notes[tuning[a] % OCTAVE] = 1
+	print notes
 
 def countCowboys(tuning):
-	for a in range(6):
-		print tuning[a]
+	for a in range(3):
+		isTriad(tuning,a)
 
 countCowboys([0,5,10,3,7,0])
